4e82b4c8fef1863841a006765a4027bfeabf4277
lisushka <lisushka@users.noreply.github.com>
2018-09-07 14:50:06 +1000

    Created bot structure, started work on commands

---
+++ main.js
@@ -0,0 +1,257 @@
+const Discord = require("discord.js");
+const config = require("./config.json");
+const constants = require("./constants.js");
+const logger = require("./logger.js");
+
+client = new Discord.client();
+
+client.on("ready", () => {
+    logger.info("Lily is online");
+});
+
+var cmdList = {
+    "hug": {
+        name: "hug",
+        description: "Hug a fellow Wrimo.",
+        usage: "@user",
+        process: function(client,msg,suffix) {
+            // hug placeholder
+
+            // hug without suffix hugs author
+
+            // hug with @ hugs @-user
+
+            // hug with another suffix hugs the text
+        }
+    },
+    "fetch": {
+        name: "fetch",
+        description: "Find an <item> for a fellow Wrimo.",
+        usage: "item @user",
+		process: function(client,msg,suffix) {
+            // fetch placeholder
+
+            // fetch without suffix fetches a random object
+
+            // fetch with @ fetches a rendom object for @-user
+
+            // fetch with item and @ fetches an item for @-user
+
+            // fetch with item fetches an item
+		}
+    },
+    "pillow": {
+        name: "pillow",
+        description: "Pillow a fellow Wrimo.",
+        usage: "@user",
+		process: function(client,msg,suffix) {
+            // pillow placeholder
+
+            // pillow without suffix pillows the user
+
+            // pillow with @ pillows the @-user
+
+            // pillow with another suffix pillows the text
+		}
+    },
+    "weather": {
+        name: "weather",
+        description: "What's the weather like today?",
+		process: function(client,msg,suffix) {
+            // weather placeholder
+		}
+    },
+    "roll": {
+        name: "roll",
+        description: "Rolls any combination of the given options,"
+            + " separated by the + operator",
+        usage: "x, x y, xdy",
+        type: "other",
+        process: function(client,msg,suffix) {
+            var diceString = "";
+            var diceSum = 0;
+            var faces = suffix.split("+");
+            for (var i = 0; i < faces.length; i++) {
+                if(Number.isInteger(Number(faces[i]))) { // Single number
+                    if (faces.length == 1) { // Treat as a 1dx roll
+                        var roll = (Math.floor(Math.random() * Number(faces[i]))
+                            + 1)
+                        diceString += roll;
+                        diceSum += roll;
+                    } else { // Add to the other rolls
+                        diceString += "(" + Number(faces[i]) + ")";
+                        diceSum += Number(faces[i]);
+                    }
+                } else if (faces[i].split("d").length == 2) { // RPG-style roll
+                    rpgRoll = faces[i].split("d");
+                    if (rpgRoll[0] == "") {
+                        rpgRoll[0] = 1;
+                    }
+                    if (!Number.isInteger(Number(rpgRoll[0])) ||
+                        !Number.isInteger(Number(rpgRoll[1]))) {
+                        diceString = "Error: Both values in an RPG-style roll"
+                            + " must be integers.";
+                        diceSum = 0;
+                        break;
+                    } else {
+                        if(rpgRoll[0] > 20) {
+                            diceString = "ERROR: TOO BIG.";
+                            diceSum = 0;
+                            break;
+                        } else {
+                            for (var j = 0; j < Number(rpgRoll[0]); j++){
+                                var roll = (Math.floor(Math.random() *
+                                    Number(rpgRoll[1])) + 1)
+                                diceString += roll;
+                                if (j < (Number(rpgRoll[0]) - 1)) {
+                                    diceString += ", "
+                                }
+                                diceSum += roll;
+                            }
+                        }
+                    }
+                } else if(faces[i].split(" ").length == 2){ // Range roll
+                    rangeRoll = faces[i].split(" ");
+                    if (!Number.isInteger(Number(rangeRoll[0])) ||
+                        !Number.isInteger(Number(rangeRoll[1]))) {
+                        diceString = "Error: Both values in a range roll"
+                            + " must be integers.";
+                        diceSum = 0;
+                        break;
+                    } else {
+                        if(Number(rangeRoll[0]) < Number(rangeRoll[1])){
+                            var roll = (Math.floor(Math.random() * (1 + Number
+                                (rangeRoll[1]) - Number(rangeRoll[0]))
+                                + Number(rangeRoll[0])));
+                            diceString += roll;
+                            diceSum += roll;
+                        } else {// First number is larger than second
+                            diceString = "Error: The first number in a range"
+                                + " roll must be smaller than the second.";
+                            diceSum = 0;
+                            break;
+                        }
+                    }
+                } else {
+                    diceString = "Error: " + faces[i] + " is not a valid roll.";
+                    diceSum = 0;
+                    break;
+                }
+                if (i < (faces.length - 1)) {
+                    diceString += ", "
+                }
+            }
+            msg.channel.send(diceString);
+            if (diceSum > 0) {
+                msg.channel.send("Total = " + diceSum);
+            }
+        }
+    },
+    "choose": {
+        name: "choose",
+        description: "Selects an item from a list <list> of items,"
+            + " separated by commas",
+        usage: "list",
+        type: "other",
+		process: function(client,msg,suffix) {
+            var items = suffix.split(",");
+            var choiceID = (Math.floor(Math.random() * items.length));
+            msg.channel.send(msg.author + ", from " + suffix + ", I selected **"
+                + items[choiceID].trim() + "**");
+		}
+    }
+}
+
+client.on('message', (msg) => {
+    if(msg.isMentioned(client.user)){
+        msg.channel.send("Hi, I'm Lily. I'm here to greet new users, fetch "
+            + " things and offer hugs.  Use !help to find out more.");
+    }
+    if(msg.author.id != client.user.id && (msg.content.startsWith(constants
+        .CMD_PREFIX))){
+        logger.info(msg.author + " entered command " + msg.content);
+        var cmdData = msg.content.split(" ")[0]
+            .substring(constants.CMD_PREFIX.length).toLowerCase();
+        var suffix = msg.content.substring(cmdData.length
+            + constants.CMD_PREFIX.length + 1)
+		var cmd = cmdList[cmdData];
+        if(cmdData === "help"){
+            if(suffix){
+                var cmd = cmdList[suffix];
+                var helpMsg = "";
+                try {
+                    helpMsg += "Data for " + cmd.name;
+                    var cmdUse = cmd.usage;
+                    if(cmdUse){
+                        helpMsg += " " + cmdUse;
+                    }
+                    var cmdDesc = cmd.description;
+                    if(cmdDesc){
+                        helpMsg += ": " + cmdDesc;
+                    }
+                    helpMsg += "\n"
+                    msg.channel.send(helpMsg);
+                } catch(e) {
+                    msg.channel.send("That command does not exist.");
+                }
+			} else {
+                msg.author.send("**Lily's Commands:**").then(function(){
+                var helpMsg = "";
+                for(var i in cmdList) {
+                    helpMsg += "**" + constants.CMD_PREFIX;
+                    var cmdName = cmdList[i].name;
+                    if(cmdName){
+						helpMsg += cmdName;
+                    }
+                    var cmdUse = cmdList[i].usage;
+                    if(cmdUse){
+						helpMsg += " " + cmdUse;
+					}
+                    var cmdDesc = cmdList[i].description;
+                    if(cmdDesc){
+						helpMsg += ":** " + cmdDesc;
+					}
+                    helpMsg += "\n";
+                }
+                msg.channel.send(msg.author + ", I sent you a DM.");
+                msg.author.send(helpMsg);	
+			});
+		    }
+        }
+		else if(cmd) {
+		    try{
+				cmd.process(client,msg,suffix);
+			} catch(e){
+                msg.channel.send("Unknown error.  See log file for details.");
+                logger.error('Error %s: %s.', e, e.stack);
+			}
+		} else {
+            msg.channel.send(cmdData + " is not a valid command."
+             + " Type !help for a list of commands.");
+		}
+	} else {
+		return
+    }
+});
+
+client.on('guildMemberAdd', member => {
+    member.guild.channels.get('channelID').send("Welcome to "
+        + member.guild.name + ", " + member + "! Please read our Code of"
+        + " Conduct in " + constants.rulesLoc + ", and introduce yourself in"
+        + constants.introLoc + ".\nThe required format for introductions can be"
+        + " found in the pinned post."); 
+});
+
+process.on('uncaughtException', function(e) {
+    logger.error("Error %s: %s.\nLily will now attempt to reconnect.",
+        e, e.stack);
+    try {
+        client.login(config.token);
+        fileSystemCheck();
+    } catch (e) {
+        logger.error("Reconnection failed.\nLily will now terminate.");
+        process.exit(1);
+    }
+  })
+
+client.login(config.token);
