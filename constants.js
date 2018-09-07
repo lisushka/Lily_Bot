7c872aa1bc8382522cbc79edc7ad64182ce15fa8
Dawn E. Collett <lisushka@users.noreply.github.com>
2018-09-07 22:32:07 +1000

    Added weather strings.

--- constants.js
+++ constants.js
@@ -1,6 +1,7 @@
 var rulesLoc = "#server-rules";
 var introLoc = "#introductions";
-const FETCH_LIST = [" rummages in a closet, and fetches ",
+const FETCH_LIST =
+    [" rummages in a closet, and fetches ",
     " dives into the ocean, and retrieves ",
     " hops into a dragon's hoard, and snatches ",
     "  ",
@@ -12,7 +13,8 @@ const FETCH_LIST = [" rummages in a closet, and fetches ",
     "  ",
     "  ",
     " slides down the slippery slope, and offers a "];
-const PILLOW_LIST = ["One of your characters receives an anonymous gift.",
+const PILLOW_LIST =
+    ["One of your characters receives an anonymous gift.",
     "Your character invites someone they don’t like over for dinner.",
     "A character is scared that someone will find out about something they did.",
     "Your character goes to the other side of town for a day.",
@@ -24,15 +26,16 @@ const PILLOW_LIST = ["One of your characters receives an anonymous gift.",
     "Your character goes to the other side of town for a day.",
     "Two characters get into a heated argument about work.",
     "A character suddenly has to travel a long distance."];
-const WEATHER_LIST = ["It's sunny outside.  Oh no, wait, it's raining now.  Typical Melbourne weather!",
-    "Do you really want to know whatthe weather is?  It's going to change in 5 minutes!",
+const WEATHER_LIST =
+    ["It's sunny outside.  Oh no, wait, it's raining now.  Typical Melbourne weather!",
+    "Do you really want to know what the weather is?  It's going to change in 5 minutes!",
     "Is that a full moon tonight?  Wow, how beautiful.",
-    "Your character goes to the other side of town for a day.",
-    "Two characters get into a heated argument about work.",
-    "A character suddenly has to travel a long distance.",
-    "Your character makes a new friend in an unexpected place.",
-    "Your characters get dressed up.",
-    "One of your characters isn’t who they claim to be.",
-    "Your character is presented with a meal they don’t like.",
+    "The weather is inconsequential.  You don't need *weather* to write!",
+    "It's overcast.  It might be day time.  I can't tell.",
+    "It's windy.  Why don't you go fly a kite?",
+    "What weather?  It's outside that window you never open.",
+    "It's sunny, with a hint of garlic in the air.  Vampires stay inside!",
+    "It's snowing!  Would you like to make a snow angel?",
+    "There are showers in the vicinity.  It's sort of chilly at eight degrees.",
     "It's very wet out there.  Submarine stocks must be soaring!",
     "It's raining cats and dogs.  That explains the noise!"];
